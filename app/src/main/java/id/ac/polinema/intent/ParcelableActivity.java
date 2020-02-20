e5d3b87c5dff5d5e6b829e69fead024d6e0cb5fb
samsul24 <samsulibs30@gmail.com>
2020-02-21 05:46:30 +0700

    menyelesakan

--- app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -1,11 +1,13 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ParcelableActivity extends AppCompatActivity {
-
+    import model.User;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -1,10 +1,15 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +17,18 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
             // TODO: display value here
+            usernameText.setText(bundle.getString("username"));
+            nameText.setText(bundle.getString("name"));
+            ageText.setText(String.valueOf(bundle.getInt("age")));
+
         }
     }
 }
